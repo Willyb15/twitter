8e4beb9833eefcf6eeb26b936b0710fbbb331a21
Willyb15 <william.steen.bryant@gmail.com>
2016-04-05 20:47:25 -0400

    Updated Angular. Doing some http get requests, receiving some JSON, linking the JSON between the controller and view.

---
+++ js/mainController.js
@@ -0,0 +1,55 @@
+var twitterApp = angular.module('twitterApp', ['ngRoute']);
+twitterApp.controller('mainController', function($scope, $http, $routeParams, $interval){
+	$scope.message = "Hello World";
+	var twitterURL = 'http://www.digitalcrafts.com/students/twitter/hashtag.php?hash=kanyewest';
+				
+	$http.get(twitterURL).success(function(data){
+		console.log(data);
+		$scope.data = data.statuses;
+		for (i=0; i<$scope.data.length; i++){
+			var time = $scope.data[i].created_at;
+			var tweetTime = new Date(time);
+			// console.log(tweetTime);
+			// console.log(typeof(tweetTime));
+			$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+			console.log($scope.data[i].tweetSeconds);
+
+			$interval(function(){
+				for(i=0; i<$scope.data.length; i++){
+					var currentDate = new Date();
+					var currentTimeInSeconds = currentDate.getTime()/1000;
+					$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds -$scope.data[i].tweetSeconds);
+				} 
+			}, 1000);
+
+		}
+	});
+
+	$scope.submit = function(){
+		event.preventDefault();
+		console.log($scope.text);
+		searchInput = $scope.text;
+		var searchURL = 'http://www.digitalcrafts.com/students/twitter/hashtag.php?hash=' + searchInput;
+		$http.get(searchURL).success(function(data){
+		console.log(data);
+		$scope.data = data.statuses;
+		for (i=0; i<$scope.data.length; i++){
+			var time = $scope.data[i].created_at;
+			var tweetTime = new Date(time);
+			// console.log(tweetTime);
+			// console.log(typeof(tweetTime));
+			$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+			console.log($scope.data[i].tweetSeconds);
+
+			$interval(function(){
+				for(i=0; i<$scope.data.length; i++){
+					var currentDate = new Date();
+					var currentTimeInSeconds = currentDate.getTime()/1000;
+					$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds -$scope.data[i].tweetSeconds);
+				} 
+			}, 1000);
+
+		}
+	});
+	};
+});
